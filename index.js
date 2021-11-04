444070c3b826490cb4e533a6cf97e64b05b11f1a
xrmethula <x.r.methula@gmail.com>
2021-11-04 19:30:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const logger = require('./middleware/logger');
+
+const app = express();
+
+
+//init middleware
+app.use(logger);
+
+
+//members API routes
+app.use('/api/members', require('./routes/api/members'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
